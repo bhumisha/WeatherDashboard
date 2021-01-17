cda700ee0bde2c206c2053321d1b0d1863c79d9d
Bhumisha Dave <bhumishadave@Bhumishas-MBP.hsd1.ca.comcast.net>
2021-01-17 06:44:29 -0800

    Added Proper Comments

--- assets/script/weather.js
+++ assets/script/weather.js
@@ -1,3 +1,4 @@
+//fetch call to get current weather and latitude / longitude for entered city 
 function getCurrentWeather(city) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=43802440c0c6de0f332937c0fa83470c")
     .then(function(response) {
@@ -5,7 +6,10 @@ function getCurrentWeather(city) {
             response.json()
             .then(function(data) 
             { 
+                //if valid city entered , get response from weather api and storing same city to localstorage
                 storeCitiesToLocalStorage(city);
+
+                //to get 5 days forecast weather
                 getForecastWeather(data.coord.lat,data.coord.lon,city); 
 
             });
@@ -13,30 +17,36 @@ function getCurrentWeather(city) {
             alert("Error: " + response.statusText);
           }
     }).catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect to Weather app " + error);
-      });
+    });
       
-  };
+};
 
-  function getForecastWeather(lat,lon,city){
+//fetch forecast weather using below api with latitude / longitude param.
+function getForecastWeather(lat,lon,city){
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=43802440c0c6de0f332937c0fa83470c")
     .then(function(response) {
         if (response.ok) {
             response.json()
             .then(function(data) 
             {
+                //Load main panel for current day weather
                 loadCurrentWeather(data,city)
+
+                //Load forecast weather for 5 days
                 loadForeCastWeather(data);
             });
         } else {
             alert("Error: " + response.statusText);
-          }
+            }
     }).catch(function(error) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect to Weather app" + error);
-      });
-  }
+    });
+}
+
+//Load localstorage cities to List panel
+loadLocalStorageCities('');
 function loadLocalStorageCities(cityName)
   {
       //Reading localstorage list if any
@@ -56,8 +66,9 @@ function loadLocalStorageCities(cityName)
         citiContainerEL.append(titleEl); 
      }
   }
-loadLocalStorageCities('');
 
+
+//Click event for citi list item which called again weather api
 $(document).on('click', '.list-group-item', function(event) {
     event.preventDefault();
     $(this).siblings(".active").removeClass('active');
@@ -66,8 +77,10 @@ $(document).on('click', '.list-group-item', function(event) {
 }); 
 
 
-//Load Current day weather
+//Load main panel for current day weather
 function loadCurrentWeather(weatherData,inputCity){
+
+    //Create HTML Controls
     var currentWeatherCardEl = $('#currentWeatherDiv');
     currentWeatherCardEl.html('');
 
@@ -78,15 +91,18 @@ function loadCurrentWeather(weatherData,inputCity){
     var uvIndex = $('<span>');
     var weathericon = $('<img>');    
 
+    //apply CSS
     currentCityH4El.addClass('card-title d-flex');
     temparatureSpanEl.addClass('card-text d-flex');
     humiditySpanEl.addClass('card-text');
     windSpeedSpanEl.addClass('card-text');
     uvIndex.addClass('card-text d-flex');
 
+    //get today's date
     var currentDate = moment().format("MM/DD/YYYY");
     var cityHeader = inputCity + "(" + currentDate + ")" ;
 
+    //set text / html contents of controls
     currentCityH4El.text(cityHeader);
     temparatureSpanEl.html("Temparature: " + weatherData.current.temp + " &#8457 ");
     humiditySpanEl.text("Humidity: "+ weatherData.current.humidity + "%");
@@ -95,6 +111,7 @@ function loadCurrentWeather(weatherData,inputCity){
     weathericon.attr('src', iconurl);
     weathericon.attr('alt','weather icon');
 
+    //Check UVIndex
     if(weatherData.current.uvi < 3)
     {
         uvIndex.html("UV Index: " + "<p class='lowUV' >  " +  weatherData.current.uvi + "</p>");
@@ -104,6 +121,7 @@ function loadCurrentWeather(weatherData,inputCity){
         uvIndex.html("UV Index: " + "<p class='highUV' >  " +  weatherData.current.uvi + "</p>");
     }
 
+    //Add all controls to main Div
     currentCityH4El.append(weathericon);
     currentWeatherCardEl.append(currentCityH4El);
     currentWeatherCardEl.append(temparatureSpanEl);
@@ -113,17 +131,21 @@ function loadCurrentWeather(weatherData,inputCity){
 }
 
 
-
+//Load forecast weather for 5 days
 function loadForeCastWeather(foreCastData){
-    var mainForeCastEl = $('#forecastWeatherDiv');
-    mainForeCastEl.html('');
-    var heading = $("<h4>");
-    heading.text("5-Day Forecast:");
-    mainForeCastEl.append(heading);
 
+    var mainForeCastEl = $('#forecastWeatherDiv');
+    var heading = $("<h4>");
     var forecastDivEl = $('<div>');
+
     forecastDivEl.addClass('d-flex');
+    heading.text("5-Day Forecast:");
+    mainForeCastEl.html('');
+    
+    mainForeCastEl.append(heading);
     mainForeCastEl.append(forecastDivEl); 
+    
+    //Create 5 bootstrap Cards to load forecast weather details for 5 days
     if(foreCastData.daily.length){
         for(i=1;i<=5;i++){
             
@@ -145,12 +167,13 @@ function loadForeCastWeather(foreCastData){
         
             var currentDate = moment.unix(dailyObj.dt).format("MM/DD/YYYY");
             h5CardTitleEl.text(currentDate);
+
+            //To get weather icon
             var iconurl = "https://openweathermap.org/img/w/" + dailyObj.weather[0].icon + ".png";
             imgCardImageEl.attr('src', iconurl);
             imgCardImageEl.attr('alt','weather icon');
 
-            var temparature = dailyObj.temp.day;
-            pCardTempTextEl.html("Temp : " + temparature + " &#8457 ");
+            pCardTempTextEl.html("Temp : " + dailyObj.temp.day + " &#8457 ");
             pCardHumTextEl.html("Humidity : " + dailyObj.humidity + " %");
 
             cardDivEl.append(h5CardTitleEl);
@@ -163,22 +186,22 @@ function loadForeCastWeather(foreCastData){
     }
 
 }
-
+//Search Button Click Event.
 var searchCityInputEl = $("#cityname");
-var formSubmitHandler = function(event) {
+SearchBtn.addEventListener("click", searchButtonClick);
+function searchButtonClick (event) {
     event.preventDefault();
-    console.log(event);
     var cityname = searchCityInputEl.val().trim();
     if (cityname) {
         searchCityInputEl.val('');
         getCurrentWeather(cityname);
-        
     } 
     else {
-        alert("Please enter a City");
+        alert("Please enter valid City / State or Country name.");
     }
 };
   
+//LocalStorage for storing city list
 function storeCitiesToLocalStorage(cityName){
         //Reading localstorage list if any
         var cityList = JSON.parse(localStorage.getItem('weatherCityList')) || [];
@@ -192,9 +215,8 @@ function storeCitiesToLocalStorage(cityName){
         if(!isCityExist){
             cityList.push(cityName);
             localStorage.setItem('weatherCityList',JSON.stringify(cityList));
+            //Load localstorage city list
             loadLocalStorageCities(cityName);
         }
-        //Save tasklist to localstorage
         
 }
-SearchBtn.addEventListener("click", formSubmitHandler);
